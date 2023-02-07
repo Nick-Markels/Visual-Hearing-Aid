06972657a7eda6f09a76fef047e999d64d9c54dd
Nick Markels <markels23@up.edu>
2023-02-06 16:00:15 -0800

    Update stereo.py
    Recording started from script as opposed from command line

--- stereo.py
+++ stereo.py
@@ -6,18 +6,19 @@ import subprocess
 pipe_path = '/tmp/audio_pipe'
 
 if not os.path.exists(pipe_path)
+    print("audio_pipe does not exist, creating FIFO...")
     os.mkfifo(pipe_path)
     
 
-
-print("hello world")
-//subprocess.run([''])
+print("Starting Recording")
+subprocess.run(['arecord -D plughw:1 -c2 -r 48000 -f S32_LE -t raw -V stereo -v /tmp/audio_pipe'])
 
 chunk_size = 8192
 fig, axs = plt.subplots(2, sharex=True)
 
 with open("/tmp/audio_pipe", "rb") as f:
     while True:
+        print("\/")
         audio_data = f.read(chunk_size)
         audio_array = np.frombuffer(audio_data, dtype=np.int32)
         left_channel = audio_array[::2]
