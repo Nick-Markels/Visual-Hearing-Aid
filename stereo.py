c3e495a1eeb7fefb14aabd2d9a42aa8453576434
samonew <59709645+samonew@users.noreply.github.com>
2023-02-07 09:32:22 -0800

    Update stereo.py
    Adding comments for my own confusion

--- stereo.py
+++ stereo.py
@@ -5,17 +5,19 @@ import subprocess
 import threading
 
 
-
+#location of audio stream
 pipe_path = '/tmp/audio_pipe'
 
+#checks if the path exists
 if not os.path.exists(pipe_path):
     print("audio_pipe does not exist, creating FIFO...")
     os.mkfifo(pipe_path)
     
-
+#begin recording
 print("Starting Recording")
 subprocess.Popen(["arecord", "-D", "plughw:1", "-c2", "-r", "48000", "-f", "S32_LE", "-t", "raw", "-V", "stereo", "-v", "/tmp/audio_pipe"])
 
+#init plotting
 chunk_size = 8192
 fig, axs = plt.subplots(2, sharex=True)
 
@@ -26,6 +28,10 @@ with open("/tmp/audio_pipe", "rb") as f:
         audio_array = np.frombuffer(audio_data, dtype=np.int32)
         left_channel = audio_array[::2]
         right_channel = audio_array[1::2]
+       
+        #decision alg goes here
+
+        #plot out the information
         axs[0].clear()
         axs[0].plot(left_channel)
         axs[1].clear()
