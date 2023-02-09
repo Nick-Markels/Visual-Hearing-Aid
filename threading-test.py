0e6db419c66e61ffd81e6f667c45dd68c52aa9b6
Nick Markels <markels23@up.edu>
2023-02-09 15:58:47 -0800

    Update threading-test.py

--- threading-test.py
+++ threading-test.py
@@ -14,24 +14,21 @@ def start_recording():
     print("Starting Recording")
     subprocess.run(["arecord", "-D", "plughw:1", "-c2", "-r", "48000", "-f", "S32_LE", "-t", "raw", "-V", "stereo", "-v", "/tmp/audio_pipe"])
 
-def plot_audio():
-    with console_lock:
-        chunk_size = 8192
-        fig, axs = plt.subplots(2, sharex=True)
-
-        with open("/tmp/audio_pipe", "rb") as f:
-            while True:
-                print("\/")
-                audio_data = f.read(chunk_size)
-                audio_array = np.frombuffer(audio_data, dtype=np.int32)
-                left_channel = audio_array[::2]
-                right_channel = audio_array[1::2]
-                axs[0].clear()
-                axs[0].plot(left_channel)
-                axs[1].clear()
-                axs[1].plot(right_channel)
-                plt.pause(0.01)
-
-# Start the recording and plotting in separate threads
 threading.Thread(target=start_recording).start()
-threading.Thread(target=plot_audio).start()
+
+chunk_size = 8192
+fig, axs = plt.subplots(2, sharex=True)
+
+with open("/tmp/audio_pipe", "rb") as f:
+    while True:
+        print("\/")
+        audio_data = f.read(chunk_size)
+        audio_array = np.frombuffer(audio_data, dtype=np.int32)
+        left_channel = audio_array[::2]
+        right_channel = audio_array[1::2]
+        axs[0].clear()
+        axs[0].plot(left_channel)
+        axs[1].clear()
+        axs[1].plot(right_channel)
+        plt.pause(0.01)
+
