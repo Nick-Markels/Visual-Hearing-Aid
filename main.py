036ff29e9639358ff5892eb720fd8f4920f370d6
Nick Markels <markels23@up.edu>
2023-02-21 17:47:31 -0800

    Merged Samone's code to main

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import os
 import subprocess
 import threading
 
+#Designate location of where audio FIFO is located
 pipe_path = '/tmp/audio_pipe'
 
+#Checks if FIFO exists, if not creates it
 if not os.path.exists(pipe_path):
     print("audio_pipe does not exist, creating FIFO...")
     os.mkfifo(pipe_path)
 
+#Sends start recording command to linux terminal
+#to start recording stereo audio to the audio FIFO
 def start_recording():
     print("Starting Recording")
     subprocess.run(["arecord", "-D", "plughw:1", "-c2", "-r", "48000", "-f", "S32_LE", "-t", "raw", "-V", "stereo", "-v", "/tmp/audio_pipe"])
+
 
 threading.Thread(target=start_recording).start()
 
@@ -31,4 +36,42 @@ with open("/tmp/audio_pipe", "rb") as f:
         axs[1].clear()
         axs[1].plot(right_channel)
         plt.pause(0.01)
+
+        #phase angle alg here
+
+
+        #decision alg goes here
+
+        #get average sound FIX
+        avg_sound = (left_channel * right_channel) /2
+        
+        #if the audio value is lower than threshold, ignore
+        for x in avg_sound:
+            if x > threshold:
+                #time since last noise - if longer than threshold, lower threshold
+                if timeSinceLast > MAX_THRESH_PERIOD:
+                    threshold -= DEC_VAL
+                
+                #if the audio value is extremely sharp, color differently
+                #plot out the information
+                #use phase-angle to get the direction of the sound
+                if noiseSlope >= SHARP:
+                    axs[0].clear()
+                    axs[0].plot(left_channel, color = "red")
+                    axs[1].clear()
+                    axs[1].plot(right_channel, color = "red")
+                    plt.pause(TIME_LIT)
+                else:
+                    axs[0].clear()
+                    axs[0].plot(left_channel, color="green")
+                    axs[1].clear()
+                    axs[1].plot(right_channel, color="green")
+                    plt.pause(TIME_LIT)
+            
+            totalNoise += x
+            sampleNum += 1
+            noiseSlope = totalNoise / sampleNum
+
+        timeSinceLast += TIME_LIT
+        
 
