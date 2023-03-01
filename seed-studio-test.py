1b5795841c0de5bf504d1f90762c6c03db3c9f2d
Nick Markels <markels23@up.edu>
2023-03-01 14:46:42 -0800

    plot 8 channels

--- seed-studio-test.py
+++ seed-studio-test.py
@@ -1,5 +1,7 @@
 import pyaudio
 import wave
+import matplotlib.pyplot as plt
+import numpy as np
  
 RESPEAKER_RATE = 16000
 RESPEAKER_CHANNELS = 8 
@@ -32,6 +34,23 @@ print("* done recording")
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+# convert the audio data to a NumPy array
+samples = np.frombuffer(b''.join(frames), dtype=np.int16)
+
+# reshape the array to an array with shape (n_channels, -1)
+samples = samples.reshape((RESPEAKER_CHANNELS, -1), order='F')
+
+# plot each channel
+t = np.arange(samples.shape[1]) / RESPEAKER_RATE
+fig, axs = plt.subplots(nrows=RESPEAKER_CHANNELS, sharex=True)
+for i in range(RESPEAKER_CHANNELS):
+    axs[i].plot(t, samples[i])
+    axs[i].set_ylabel(f'Channel {i+1}')
+axs[-1].set_xlabel('Time (s)')
+
+plt.show()
+
  
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(RESPEAKER_CHANNELS)
