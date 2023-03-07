f3b38b0bcec0f0972273d8416de7cfcda53450dc
Nick Markels <markels23@up.edu>
2023-03-07 15:22:13 -0800

    ,,

--- seed-studio-default.py
+++ seed-studio-default.py
@@ -3,12 +3,12 @@ import wave
 import numpy as np
 
 RESPEAKER_RATE = 16000
-RESPEAKER_CHANNELS = 8
+RESPEAKER_CHANNELS = 8 
 RESPEAKER_WIDTH = 2
 # run getDeviceInfo.py to get index
 RESPEAKER_INDEX = 2  # refer to input device id
 CHUNK = 1024
-RECORD_SECONDS = 3
+RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -22,13 +22,21 @@ stream = p.open(
 
 print("* recording")
 
-frames = [] 
+frames = []
 
 for i in range(0, int(RESPEAKER_RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
+    frames.append(data)
     # extract channel 0 data from 8 channels, if you want to extract channel 1, please change to [1::8]
-    a = np.fromstring(data,dtype=np.int16)[0::8]
-    frames.append(a.tostring())
+    ch0 = np.fromstring(data,dtype=np.int16)[0::8]
+    ch1 = np.fromstring(data,dtype=np.int16)[1::8]
+    ch2 = np.fromstring(data,dtype=np.int16)[2::8]
+    ch3 = np.fromstring(data,dtype=np.int16)[3::8]
+    ch4 = np.fromstring(data,dtype=np.int16)[4::8]
+    ch5 = np.fromstring(data,dtype=np.int16)[5::8]
+    ch6 = np.fromstring(data,dtype=np.int16)[6::8]
+    ch7 = np.fromstring(data,dtype=np.int16)[7::8]
+    #frames.append(a.tostring())
 
 print("* done recording")
 
@@ -37,7 +45,7 @@ stream.close()
 p.terminate()
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(1)
+wf.setnchannels(RESPEAKER_CHANNELS)
 wf.setsampwidth(p.get_sample_size(p.get_format_from_width(RESPEAKER_WIDTH)))
 wf.setframerate(RESPEAKER_RATE)
 wf.writeframes(b''.join(frames))
--- seed-studio-test.py
+++ seed-studio-test.py
@@ -1,15 +1,14 @@
 import pyaudio
 import wave
 import numpy as np
-import matplotlib.pyplot as plt
 
 RESPEAKER_RATE = 16000
-RESPEAKER_CHANNELS = 8
+RESPEAKER_CHANNELS = 8 
 RESPEAKER_WIDTH = 2
 # run getDeviceInfo.py to get index
 RESPEAKER_INDEX = 2  # refer to input device id
 CHUNK = 1024
-RECORD_SECONDS = 3
+RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -23,16 +22,25 @@ stream = p.open(
 
 print("* recording")
 
-frames = [] 
-my_list = []
-my_list = np.array(my_list)
+frames = []
+myArray = []
+myArray = np.array(myArray)
 
 for i in range(0, int(RESPEAKER_RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
+    frames.append(data)
     # extract channel 0 data from 8 channels, if you want to extract channel 1, please change to [1::8]
-    a = np.fromstring(data,dtype=np.int16)[0::8]
-    frames.append(a.tostring())
-    my_list = np.append(my_list, a)
+    ch0 = np.fromstring(data,dtype=np.int16)[0::8]
+    ch1 = np.fromstring(data,dtype=np.int16)[1::8]
+    ch2 = np.fromstring(data,dtype=np.int16)[2::8]
+    ch3 = np.fromstring(data,dtype=np.int16)[3::8]
+    ch4 = np.fromstring(data,dtype=np.int16)[4::8]
+    ch5 = np.fromstring(data,dtype=np.int16)[5::8]
+    ch6 = np.fromstring(data,dtype=np.int16)[6::8]
+    ch7 = np.fromstring(data,dtype=np.int16)[7::8]
+
+    tmp = np.vstack(ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7)
+    myArray = np.append(myArray, tmp)
 
 print("* done recording")
 
@@ -40,18 +48,8 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
-fig, ax = plt.subplots(8)
-length3 = int(len(my_list)/ RESPEAKER_CHANNELS)
-print("Shape of the data array: ", my_list.shape)
-my_list = my_list.reshape((RESPEAKER_CHANNELS, length3))
-
-for i in range(my_list.shape[0]):
-    ax[i].plot(my_list[i], label="Channel {}".format(i+1))
-
-fig.savefig("p.png")
-
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(1)
+wf.setnchannels(RESPEAKER_CHANNELS)
 wf.setsampwidth(p.get_sample_size(p.get_format_from_width(RESPEAKER_WIDTH)))
 wf.setframerate(RESPEAKER_RATE)
 wf.writeframes(b''.join(frames))
