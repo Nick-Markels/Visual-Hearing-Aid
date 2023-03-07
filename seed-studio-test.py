5d05e44349a7d574338538c8b1c890cbc0e77d0f
Nick Markels <markels23@up.edu>
2023-03-07 14:40:33 -0800

    .

--- seed-studio-test.py
+++ seed-studio-test.py
@@ -1,7 +1,7 @@
 import pyaudio
 import wave
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 RESPEAKER_RATE = 16000
 RESPEAKER_CHANNELS = 8
@@ -11,7 +11,6 @@ RESPEAKER_INDEX = 2  # refer to input device id
 CHUNK = 1024
 RECORD_SECONDS = 3
 WAVE_OUTPUT_FILENAME = "output.wav"
-PNG_OUTPUT_FILENAME = "output.png"
 
 p = pyaudio.PyAudio()
 
@@ -28,9 +27,9 @@ frames = []
 
 for i in range(0, int(RESPEAKER_RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    # extract all 8 channels using numpy
+    # extract channel 0 data from 8 channels, if you want to extract channel 1, please change to [1::8]
     a = np.fromstring(data,dtype=np.int16)[0::8]
-    frames.append(a)
+    frames.append(a.tostring())
 
 print("* done recording")
 
@@ -38,19 +37,7 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
-# create a new figure
-fig, axs = plt.subplots(RESPEAKER_CHANNELS, 1, figsize=(10, 15), sharex=True)
 
-# plot each channel on its own subplot
-for i in range(RESPEAKER_CHANNELS):
-    axs[i].plot(frames[-1][i::RESPEAKER_CHANNELS])
-    axs[i].set_ylabel("Channel {}".format(i+1))
-
-# set the x-label on the bottom subplot
-axs[-1].set_xlabel("Time (samples)")
-
-# save the figure to a PNG file
-plt.savefig(PNG_OUTPUT_FILENAME)
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(1)
