a85f9db98f126a0027a1322113c3afea150d6e11
Nick Markels <markels23@up.edu>
2023-03-08 17:29:09 -0800

    ,,

--- seed-studio-script.py
+++ seed-studio-script.py
@@ -3,15 +3,15 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
+#starting values
 RESPEAKER_RATE = 16000
 RESPEAKER_CHANNELS = 8 
 RESPEAKER_WIDTH = 2
-# run getDeviceInfo.py to get index
 RESPEAKER_INDEX = 2  # refer to input device id
 CHUNK = 1024
-RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
+#opens audio stream
 p = pyaudio.PyAudio()
 stream = p.open(
             rate=RESPEAKER_RATE,
@@ -27,9 +27,14 @@ myArray = []
 myArray = np.array(myArray)
 fig, axs = plt.subplots(8, sharex=True)
 
+
+#starts infinite loop
 while True:
+
+    #reads chunk of data from audio stream
     data = stream.read(CHUNK, exception_on_overflow = False)
     frames.append(data)
+
     # extract channel 0 data from 8 channels, if you want to extract channel 1, please change to [1::8]
     ch0 = np.fromstring(data,dtype=np.int16)[0::8]
     ch1 = np.fromstring(data,dtype=np.int16)[1::8]
@@ -40,11 +45,10 @@ while True:
     ch6 = np.fromstring(data,dtype=np.int16)[6::8]
     ch7 = np.fromstring(data,dtype=np.int16)[7::8]
 
-    tmp = np.vstack((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7))
-    #myArray = np.hstack((myArray, tmp))
-    myArray = tmp
-
+    #creates array of data from 8 channels
+    myArray = np.vstack((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7))
    
+   #plots data
     for i in range(8):
         axs[i].clear()
         axs[i].plot(myArray[i])
